abcd8999ba6085949d2da52862b42033c203b80f
CoralieEspasa <coralie.espasa@gmail.com>
2021-02-01 10:22:32 +0100

    initial commit

---
+++ hackathon-artilect-big-bro/artilectBigBro/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using artilectBigBro.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace artilectBigBro.Data
+{
+	public class ApplicationDbContext : IdentityDbContext<User>
+	{
+		public DbSet<Interest> Interests { get; set; }
+		public DbSet<Skill> Skills { get; set; }
+		public DbSet<Machine> Machines { get; set; }
+		public DbSet<Project> Projects { get; set; }
+		public DbSet<Badge> Badges { get; set; }
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+    }
+}
